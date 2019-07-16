06eab4b14861cb5d8a45cfb5959502d278717483
wiginzeng <wjzeng@shmiic.com>
2019-07-16 14:55:21 +0800

    typetest: add process logic
    Signed-off-by: wiginzeng <wjzeng@shmiic.com>

--- arm/Siemens/V2.0/typeset_test/USER/mvb_typeset_test.c
+++ arm/Siemens/V2.0/typeset_test/USER/mvb_typeset_test.c
@@ -97,7 +97,7 @@ void timer_isr(void)
 	event_check(3394_3380_port); 
 }
 
-void test_host_failure(void)
+static void test_host_failure(void)
 {
 	/*need fire watchdog*/
 	wdt_fired();
@@ -106,12 +106,134 @@ void test_host_failure(void)
 int process_msg_from_3368(struct 3368_msg_t * msg)
 {
 	if (msg == NULL) return -1;
+	memcpy(msg, &3360_msg, sizeof(3360_msg));
+	/*reset err/warning*/
+	if (msg->INT16_2) {
+		3360_msg.UINT16_2 = 0;
+		3360_msg.UINT16_3 = 0;
+	}
+	/*Host failure*/
 	if(!msg->BOOLEAN1)
 		test_host_failure();
+	else {
+		3360_msg.INT16_1++;
+		3360_msg.INT16_2++;
+		3360_msg.UINT16_1++;
+		3360_msg.BOOLEAN_1++;
+		3360_msg.BOOLEAN_2++;
+		reply_msg_to_zsg(3368_3360_port, &3360_msg, sizeof(3360_msg));
+	}
+	return 0;
+}
+
+int process_msg_from_3390(struct 3390_msg_t * msg)
+{
+	if (msg == NULL) return -1;
+	memcpy(msg, &3376_msg, sizeof(3376_msg));
+	3376_msg.INT16_1++;
+	3376_msg.UINT8_1++;
+	3376_msg.INT8_1++;
+	3376_msg.CHAR1++;
+	reply_msg_to_zsg(3390_3376_port, &3376_msg, sizeof(3376_msg));
+	return 0;
+}
+
+int process_msg_from_3391(struct 3391_msg_t * msg)
+{
+	if (msg == NULL) return -1;
+	memcpy(msg, &3377_msg, sizeof(3377_msg));
+	3377_msg.INT16_1++;
+	3377_msg.UINT8_1++;
+	3377_msg.UINT32_1++;
+	3377_msg.INT32_1++;
+	3377_msg.CHAR1++;
+	3377_msg.BOOLEAN_1++;
+	3377_msg.BOOLEAN_2++;
+	reply_msg_to_zsg(3391_3377_port, &3377_msg, sizeof(3377_msg));
+	return 0;
 
 }
 
-void typeset_test_service(void) 
+int process_msg_from_3392(struct 3392_msg_t * msg)
 {
+	if (msg == NULL) return -1;
+	memcpy(msg, &3378_msg, sizeof(3378_msg));
+	3378_msg.INT16_1++;
+	reply_msg_to_zsg(3392_3378_port, &3378_msg, sizeof(3378_msg));
+	return 0;
+}
 
+int process_msg_from_3393(struct 3393_msg_t * msg)
+{
+	if (msg == NULL) return -1;
+	memcpy(msg, &3379_msg, sizeof(3379_msg));
+	3379_msg.INT16_1++;
+	reply_msg_to_zsg(3393_3379_port, &3379_msg, sizeof(3379_msg));
+	return 0;
+}
+
+int process_msg_from_3394(struct 3394_msg_t * msg)
+{
+	if (msg == NULL) return -1;
+	memcpy(msg, &3380_msg, sizeof(3380_msg));
+	3380_msg.INT16_1++;
+	3380_msg.CHAR1++;
+	3380_msg.UINT32_1++;
+	3380_msg.UINT32_2++;
+	reply_msg_to_zsg(3394_3380_port, &3380_msg, sizeof(3380_msg));
+	return 0;
+}
+
+void init_typeset_service(void) 
+{
+	3368_3360_port = &3368_3360_group;
+	3390_3376_port = &3390_3376_group;
+	3391_3377_port = &3391_3377_group;
+	3392_3378_port = &3392_3378_group;
+	3393_3379_port = &3393_3379_group;
+	3394_3380_port = &3394_3380_group;
+}
+
+void mvb_typetest_service(void) 
+{
+	if(3368_3360_port->is_req_event) {
+		copy_req_msg(3368_3360_port,
+					&3368_msg,
+					sizeof(3368_msg));
+		process_msg_from_3368(&3368_msg);
+	}
+	
+	if(3390_3376_port->is_req_event) {
+		copy_req_msg(3390_3376_port,
+					&3390_msg,
+					sizeof(3390_msg));
+		process_msg_from_3390(&3390_msg);
+	}
+	
+	if(3391_3377_port->is_req_event) {
+		copy_req_msg(3391_3377_port,
+					&3391_msg,
+					sizeof(3391_msg));
+		process_msg_from_3391(&3391_msg);
+	}
+	
+	if(3392_3378_port->is_req_event) {
+		copy_req_msg(3392_3378_port,
+					&3392_msg,
+					sizeof(3392_msg));
+		process_msg_from_3392(&3392_msg);
+	}
+	
+	if(3393_3379_port->is_req_event) {
+		copy_req_msg(3393_3379_port,
+					&3393_msg,
+					sizeof(3393_msg));
+		process_msg_from_3393(&3393_msg);
+	}
+	if(3394_3380_port->is_req_event) {
+		copy_req_msg(3394_3380_port,
+					&3394_msg,
+					sizeof(3394_msg));
+		process_msg_from_3394(&3394_msg);
+	}
 }
--- arm/Siemens/V2.0/typeset_test/USER/mvb_typeset_test.h
+++ arm/Siemens/V2.0/typeset_test/USER/mvb_typeset_test.h
@@ -56,6 +56,22 @@ struct 3368_msg_t {
 	uint8_t	 BITSET8;
 } 3368_msg;
 
+#define DEFAULT_3360_MSG \
+{ 0X7f7f,\
+	0,\
+	0xA1A1,\
+	0xA,\
+	0x3,\
+	0x1,\
+	0x1,\
+	0xA1,\
+	0xFFFF,\
+	0xFFFF,\
+	0xA2,\
+	0xf,\
+	0xB,\
+	0X7f,\
+\}
 struct 3360_msg_t {
 	int16_t INT16_1;
 	int16_t INT16_2;
@@ -71,7 +87,7 @@ struct 3360_msg_t {
 	uint8_t  2xVALID:4;
 	uint8_t	 ENUM4:4;
 	uint8_t	 BITSET8;
-}3360_msg;
+}3360_msg = DEFAULT_3360_MSG;
 
 /*3390<-->3376*/
 #define PORTCYC_3390_3376	(64)
@@ -87,6 +103,17 @@ struct 3390_msg_t {
 	uint8_t ENUM4:4;
 }3390_msg;
 
+#define DEFAULT_3376_MSG \
+{	0x7F7F,\
+	0xA1,\
+	0xA2,\
+	0xA3,\
+	0x7F,\
+	0x21,\
+	0xB,\
+	0xA,\
+\}
+
 struct 3376_msg_t {
 	uint16_t INT16_1;
 	uint8_t BITSET8;
@@ -96,7 +123,7 @@ struct 3376_msg_t {
 	char CHAR1;
 	uint8_t BCD4:4;
 	uint8_t ENUM4:4;
-}3376_msg;
+}3376_msg = DEFAULT_3376_MSG;
 
 /*3391 <---> 3377*/
 #define PORTCYC_3391_3377	(128)
@@ -117,8 +144,27 @@ struct 3391_msg_t {
 	uint16_t TICKS;
 	uint8_t BCD4:4;
 	uint8_t ENUM4:4;
-	uint8_t UNINT8_1;
+	uint8_t UINT8_1;
 }3391_msg;
+
+#define DEFAULT_3377_MSG \
+{	0x7f7f,\
+	0x21,\
+	0xA,\
+	0x3,\
+	0x1,\
+	0x1,\
+	0xA1A1A1,\
+	0xA1A1,\
+	"ABCDEFGHIJ",\
+	0x7FFFFFFF,\
+	0xA2A2,\
+	0xA3A3,\
+	0,\
+	0XC,\
+	0xB,\
+	0xA1,\
+\}
 
 struct 3377_msg_t {
 	int16_t INT16_1;
@@ -136,8 +182,8 @@ struct 3377_msg_t {
 	uint16_t TICKS;
 	uint8_t BCD4:4;
 	uint8_t ENUM4:4;
-	uint8_t UNINT8_1;
-}3377_msg;
+	uint8_t UINT8_1;
+}3377_msg = DEFAULT_3377_MSG;
 
 
 /*3392 <---> 3378*/
@@ -150,12 +196,18 @@ struct 3392_msg_t {
 	uint8_t BITSET8;
 }3392_msg;
 
+#define DEFAULT_3378_MSG \
+{	0x3f,\
+	0x3,\
+	0xa1,\
+} 
+
 struct 3378_msg_t {
 	int16_t INT16_1;
 	uint8_t ANTIVALENT2:2;
 	uint8_t 3xVALID:6;
 	uint8_t BITSET8;
-}3378_msg;
+}3378_msg = DEFAULT_3378_MSG;
 
 /*3393 <---> 3379*/
 #define PORTCYC_3393_3379	(512)
@@ -172,6 +224,17 @@ struct 3393_msg_t {
 	char STR16[16];
 }3393_msg;
 
+#define DEFAULT_3379_MSG \
+{	0x7f7f,\
+	0xa1a1,\
+	"ABC0",\
+	"ABCD",\
+	0x7FFF,\
+	0x5,\
+	0xa,\
+	0x55,\
+	"ABCDEFGHIJKLMNOP",\
+}
 struct 3379_msg_t {
 	int16_t INT16_1;
 	uint16_t BITSET16;
@@ -182,7 +245,7 @@ struct 3379_msg_t {
 	uint8_t 2xVALID:4;
 	uint8_t 4xVALID;
 	char STR16[16];
-}3379_msg;
+}3379_msg = DEFAULT_3379_MSG;
 
 /*3394 <---> 3380*/
 #define PORTCYC_3394_3380	(1024)
@@ -196,6 +259,13 @@ struct 3394_msg_t {
 	uint32_t UINT32_2;
 }3394_msg;
 
+#define DEFAULT_3380_MSG \
+{	0x55,\
+	0x21,\
+	0xA1A1A1A1,\
+	0xA2A2A2A2,\
+	0xA3A3A3A3,\
+}
 struct 3380_msg_t {
 	int16_t INT16_1;
 	uint8_t 4xVALID;
@@ -203,6 +273,6 @@ struct 3380_msg_t {
 	uint32_t BITSET32;
 	uint32_t UINT32_1;
 	uint32_t UINT32_2;
-}3380_msg;
+}3380_msg = DEFAULT_3380_MSG;
 #endif
 
